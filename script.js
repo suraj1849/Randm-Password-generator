89dbc045da8a2387646cfb6ad74c215b6679f2f5
Suraj Patil <113511921+suraj1849@users.noreply.github.com>
2024-02-01 00:14:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-const inputSlider = document.querySelector("[data-lengthSlider]");
-const lengthDisplay = document.querySelector("[data-lengthNumber]");
-
-const passwordDisplay = document.querySelector("[data-passwordDisplay]");
-const copyBtn = document.querySelector("[data-copy]");
-const copyMsg = document.querySelector("[data-copyMsg]");
-const uppercaseCheck = document.querySelector("#uppercase");
-const lowercaseCheck = document.querySelector("#lowercase");
-const numbersCheck = document.querySelector("#numbers");
-const symbolsCheck = document.querySelector("#symbols");
-const indicator = document.querySelector("[data-indicator]");
-const generateBtn = document.querySelector(".generateButton");
-const allCheckBox = document.querySelectorAll("input[type=checkbox]");
-const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
 
 
 //initially
